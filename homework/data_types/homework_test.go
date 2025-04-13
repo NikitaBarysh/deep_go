cfe430001adb52b73c6e236e46066706eeca21eb
Nikita Baryshnikov <baryshnikov.n14@wb.ru>
2025-04-13 13:08:45 +0300

    data_types HW

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+func ToLittleEndian[T ~uint16 | uint32 | ~uint64](number T) T {
+	size := unsafe.Sizeof(number)
+	pointer := unsafe.Pointer(&number)
+
+	for i := 0; i < int(size)/2; i++ {
+		low := (*byte)(unsafe.Add(pointer, i))
+		high := (*byte)(unsafe.Add(pointer, int(size)-1-i))
+		*low, *high = *high, *low
+	}
+
+	return number
 }
 
-func TestСonversion(t *testing.T) {
+func TestSerializationProperties32(t *testing.T) {
 	tests := map[string]struct {
 		number uint32
 		result uint32
@@ -36,6 +46,41 @@ func TestСonversion(t *testing.T) {
 		"test case #5": {
 			number: 0x01020304,
 			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestSerializationProperties16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF,
+			result: 0xFFFF,
+		},
+		"test case #5": {
+			number: 0x0102,
+			result: 0x0201,
 		},
 	}
 
