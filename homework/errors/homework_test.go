e1a1501b00850a8136320f133d78a500d09b3ff5
Nikita Baryshnikov <baryshnikov.n14@wb.ru>
2025-06-07 17:55:15 +0300

    error HW

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +12,43 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errors []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	if e == nil || len(e.errors) == 0 {
+		return ""
+	}
+	var b strings.Builder
+
+	b.WriteString(strconv.Itoa(len(e.errors)))
+	b.WriteString(" errors occured:\n")
+	for _, err := range e.errors {
+		b.WriteString("\t* ")
+		b.WriteString(err.Error())
+	}
+	b.WriteString("\n")
+	return b.String()
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	var result *MultiError
+
+	if err == nil {
+		result = &MultiError{}
+	} else {
+		if customErr, ok := err.(*MultiError); ok && customErr != nil {
+			result = &MultiError{
+				errors: append([]error{}, customErr.errors...),
+			}
+		} else {
+			result = &MultiError{errors: []error{err}}
+		}
+	}
+
+	result.errors = append(result.errors, errs...)
+
+	return result
 }
 
 func TestMultiError(t *testing.T) {
